d6313cde64d9e98f8958f139e13fce321686f20a
rio-raj-006 <101085627+rio-raj-006@users.noreply.github.com>
2022-11-14 21:07:54 +0530

    Update mqtt.js

--- Project Development Phase/sprint-3/mqtt.js
+++ Project Development Phase/sprint-3/mqtt.js
@@ -1,1 +1,24 @@
+console.log("first")
+var client = new Paho.MQTT.Client("test.mosquitto.org", 8080, "clientId");
+console.log("Connected")
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
 
+client.connect({ onSuccess: onConnect });
+
+function onConnect() {
+  console.log("onConnect");
+  client.subscribe("sensor/distance");
+
+}
+function onConnectionLost(responseObject) {
+  if (responseObject.errorCode !== 0) {
+    console.log("onConnectionLost:" + responseObject.errorMessage);
+  }
+}
+
+function onMessageArrived(message) {
+  console.log(Number(message.payloadString));
+  const h1 = document.getElementsByClassName("random")[0];
+  h1.innerHTML = message.payloadString;
+}
